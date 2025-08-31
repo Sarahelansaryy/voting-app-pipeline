6e95d8ef30c8bdecfca30ed947d54b79452ebfae
sarah <ibrahimsarah839@gmail.com>
2025-08-31 15:52:28 +0300

    dynamo db name change

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "voting-bucket-sarah-0123"
     key            = "stage/eks/terraform.tfstate"
     region         = "eu-central-1"
-    dynamodb_table = "terraform-locks"
+    dynamodb_table = "terraform-locks-sarah"
     encrypt        = true
   }
 }
