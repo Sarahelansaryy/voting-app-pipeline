2e2ef4688229f653b66e13dad08b705ef04cf8eb
sarah <ibrahimsarah839@gmail.com>
2025-08-31 16:38:32 +0300

    irsa for helm

--- Terraform/Alb.tf
+++ Terraform/Alb.tf
@@ -1,32 +1,58 @@
-# provider "kubernetes" {
-#   host                   = module.eks.cluster_endpoint
-#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority)
-#   exec {
-#     api_version = "client.authentication.k8s.io/v1beta1"
-#     command     = "aws"
-#     args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-#   }
-# }
+#####################################
+# AWS Load Balancer Controller IRSA #
+#####################################
 
-# provider "helm" {
-#   alias = "alb"
-#   kubernetes = {
-#     host                   = module.eks.cluster_endpoint
-#     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority)
-#     exec = {
-#       api_version = "client.authentication.k8s.io/v1beta1"
-#       command     = "aws"
-#       args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-#     }
-#   }
-# }
+data "aws_iam_policy_document" "alb_assume" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
 
+    principals {
+      type        = "Federated"
+      identifiers = [module.eks.oidc_provider_arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(module.eks.oidc_provider_url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
+
+resource "aws_iam_role" "alb_irsa" {
+  name               = "${var.cluster_name}-alb-irsa"
+  assume_role_policy = data.aws_iam_policy_document.alb_assume.json
+}
+
+resource "aws_iam_role_policy_attachment" "alb_policy_attach" {
+  policy_arn = "arn:aws:iam::aws:policy/AWSLoadBalancerControllerIAMPolicy"
+  role       = aws_iam_role.alb_irsa.name
+}
+
+#####################################
+# Kubernetes Service Account (IRSA) #
+#####################################
+
+resource "kubernetes_service_account" "alb_sa" {
+  metadata {
+    name      = "aws-load-balancer-controller"
+    namespace = "kube-system"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = aws_iam_role.alb_irsa.arn
+    }
+  }
+}
+
+#####################################
+# Helm Release: AWS LB Controller   #
+#####################################
 
 resource "helm_release" "aws_load_balancer_controller" {
   name       = "aws-load-balancer-controller"
   namespace  = "kube-system"
   repository = "https://aws.github.io/eks-charts"
   chart      = "aws-load-balancer-controller"
+
   values = [
     templatefile("${path.root}/../k8s/alb-values.yaml", {
       vpc_id       = module.vpc.vpc_id
@@ -35,28 +61,16 @@ resource "helm_release" "aws_load_balancer_controller" {
     })
   ]
 
-}
-resource "aws_iam_role" "aws_load_balancer_controller" {
-  name = "${var.cluster_name}-aws-load-balancer-controller"
+  # Tell Helm to use the pre-created ServiceAccount
+  set = [{
+    name  = "serviceAccount.create"
+    value = "false"
+    }, {
+    name = "serviceAccount.name"
+    value = kubernetes_service_account.alb_sa.metadata[0].name }
+  ]
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Federated = module.eks.oidc_provider_url
-      },
-      Action = "sts:AssumeRoleWithWebIdentity",
-      Condition = {
-        StringEquals = {
-          "${replace(module.eks.oidc_provider_url, "https://", "")}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
-        }
-      }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "aws_load_balancer_controller_attach" {
-  policy_arn = "arn:aws:iam::aws:policy/AWSLoadBalancerControllerIAMPolicy"
-  role       = aws_iam_role.aws_load_balancer_controller.name
+  depends_on = [
+    kubernetes_service_account.alb_sa
+  ]
 }
